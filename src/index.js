ba0fce9fcf53363d7c280c2d13cebe133b228415
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-02-21 13:58:29 +0000

    added lat long code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        // THIS IS THE ONLY TIME we do direct assignment. Every other place should call setState
+        this.state = { 
+            loading: true,
+            lat: null,
+            errorMessage: '',
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => { 
+                this.setState({
+                    ...this.state,
+                    loading: false,
+                    lat: position.coords.latitude,
+                    errorMessage: '',
+                })
+            },
+            (error) => {
+                console.error(error);
+                this.setState({
+                    ...this.state,
+                    loading: false,
+                    lat: null,
+                    errorMessage: error.message,
+                })
+            },
+        );
+    }
+
+    render() {
+        if(this.state.loading) {
+            return <div> Loading... </div> 
+        } else if(this.state.errorMessage && !this.state.lat) {
+            return <div> Error : {this.state.errorMessage} </div>
+        } else if(this.state.lat && !this.state.errorMessage) {
+            return <div> Latitude : {this.state.lat} </div>
+        } 
+    }
+
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />, 
+    document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
