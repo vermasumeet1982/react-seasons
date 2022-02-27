1661a93a746359ad6078771cb84b2fd74b39645e
vermasumeet1982 <vermasumeet1982@gmail.com>
2022-02-27 22:17:05 +0000

    styling and spinner

---
+++ src/components/LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+    return (
+        <div className="ui active dimmer">
+            <div className="ui big text loader">
+                Loading...
+            </div>
+        </div>
+    );
+}
+
+export default LoadingSpinner;
